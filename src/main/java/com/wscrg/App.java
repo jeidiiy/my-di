47065cff62e095f88e2ad683e3cb69c5a71a87a8
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-09-28 23:50:59 +0900

    setup: 프로젝트 초기 개발 환경

---
+++ src/main/java/com/wscrg/App.java
@@ -0,0 +1,13 @@
+package com.wscrg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
