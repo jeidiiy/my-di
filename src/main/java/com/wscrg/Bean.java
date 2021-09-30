0e2da0b8969bc822ae1b202731976731420c4e5a
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-09-30 21:58:18 +0900

    feat: 자동 빈 등록을 위한 어노테이션

---
+++ src/main/java/com/wscrg/Bean.java
@@ -0,0 +1,12 @@
+package com.wscrg;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ TYPE, METHOD })
+public @interface Bean {
+}
