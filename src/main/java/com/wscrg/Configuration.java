61d6de34d38537cb93206df93ebc64b210e9ddc9
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-09-30 22:10:51 +0900

    feat: 자동 빈 주입을 위한 어노테이션

---
+++ src/main/java/com/wscrg/Configuration.java
@@ -0,0 +1,12 @@
+package com.wscrg;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Component
+public @interface Configuration {
+}
